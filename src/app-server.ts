842745ac2ba7490e770a5411b5703d23cb23f40d
Menziess <stefan_schenk@hotmail.com>
2018-01-07 20:06:31 +0100

    Attempt to fix absolute path error in heroku.

--- src/app-server.ts
+++ src/app-server.ts
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // universal routing and rendering
 app.get('*', (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile("index.html", { root: __dirname });
 });
 
 // start the server
