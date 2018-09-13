2058655cc9e52171ec423e5ccc27c7f8e97128e0
Menziess <stefan_schenk@hotmail.com>
2018-09-13 16:28:44 +0200

    Update: fixed linting.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import VueRouter from "vue-router";
 import App from "./components/App.vue";
-import 'vuetify/dist/vuetify.min.css';
+import "vuetify/dist/vuetify.min.css";
 
 import Chat1 from "./components/views/Chat1.vue";
 import Chat2 from "./components/views/Chat2.vue";
@@ -15,28 +15,28 @@ Vue.use(VueRouter);
 
 const routes: any[] = [
   {
-    path: '/',
+    path: "/",
     component: Chat1,
   },
   {
-    path: '/v1',
+    path: "/v1",
     component: Chat1,
   },
   {
-    path: '/v2',
+    path: "/v2",
     component: Chat2,
   },
 ];
 
 
 
-const router = new VueRouter({ routes, mode: 'history' });
+const router = new VueRouter({ routes, mode: "history" });
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
   render(h) {
-    return h('App');
+    return h("App");
   }
 });
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 // declare function require(name: string);
 
-import * as path from 'path';
-import * as fallback from 'connect-history-api-fallback';
-import * as express from 'express';
+import * as path from "path";
+import * as fallback from "connect-history-api-fallback";
+import * as express from "express";
 
 // initialize the server and configure support for ejs templates
 const app: express.Express = express();
@@ -12,30 +12,30 @@ app.use(
   fallback({
     rewrites: [],
     disableDotRule: true,
-    index: 'index.html',
+    index: "index.html",
     htmlAcceptHeaders: [
-      'text/html',
-      'application/xhtml+xml'
+      "text/html",
+      "application/xhtml+xml"
     ]
   })
 );
 
 // define the folder that will be used for public assets
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // universal routing and rendering
-app.get('/', (req: any, res: any) => {
+app.get("/", (req: any, res: any) => {
   res.render("index.html");
 });
 
 // start the server
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || "production";
 app.listen(port, () => {
   console.info(
-    'Server listening on http://localhost:'
+    "Server listening on http://localhost:"
     + port
-    + ', Ctrl+C to stop'
+    + ", Ctrl+C to stop"
   )
 });
 
